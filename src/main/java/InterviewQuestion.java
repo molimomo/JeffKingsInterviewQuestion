906502ae31a8a213331a3ffde97b579c9523a035
molimomo <molimomo0@gmail.com>
2015-04-29 21:45:29 -0500

    The first version of Solution for MModal Interview Question.

--- src/main/java/InterviewQuestion.java
+++ src/main/java/InterviewQuestion.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.StringReader;
+
 
 public class InterviewQuestion {
 	
@@ -10,7 +13,29 @@ public class InterviewQuestion {
 	 * @return a space separated string of words in reverse order from the input
 	 */
 	public String reverseWords(String input){
-		throw new RuntimeException("Not Implemented");
+		
+		// split the input string and the delimiter is white space.
+		String[] words = input.split(" ");
+		
+		// we use a StringBuffer to store result.
+		StringBuffer result = new StringBuffer();
+		
+		// generate result from split string.
+		for(int i=0;i<words.length;i++)
+			result.insert(0, " "+ words[i]);
+		
+		// delete the first redundant space
+		result.deleteCharAt(0);
+		
+		// return result
+		return result.toString();
 	}
 
+	public static void main(String argc[]){
+        InterviewQuestion quest = new InterviewQuestion();
+        String result = quest.reverseWords("foo bar baz");
+        System.out.println(result);
+    }
+   
 }
+
